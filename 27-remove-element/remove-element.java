06740668ded8e09f4e57ae376f9b600550fbb302
Piyush Verma <piyushsuhani05@gmail.com>
2026-09-04 13:24:01 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (79.03%) - LeetSync

--- 27-remove-element/remove-element.java
+++ 27-remove-element/remove-element.java
@@ -1,27 +1,12 @@
 class Solution {
-    public int removeElement(int[] nums, int val){
-        int count = 0;
-        for(int i=0;i<nums.length;i++)
-        {
-            if(nums[i]==val)
-            {
-                count++;
+    public int removeElement(int[] nums, int val) {
+        int i = 0; // pointer for writing
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != val) {
+                nums[i] = nums[j];
+                i++;
             }
         }
-        int n = nums.length - count;
-        int [] arr = new int[n];
-        int x = 0;
-        for(int i=0;i<nums.length;i++)
-        {
-            if(nums[i]!=val)
-            {
-                arr[x++] = nums[i];
-            }
-        }
-        for(int i=0;i<n;i++)
-        {
-            nums[i] = arr[i];
-        }
-        return arr.length;
-}
+        return i; // new length
+    }
 }
