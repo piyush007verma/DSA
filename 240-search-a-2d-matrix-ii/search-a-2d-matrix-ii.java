2c0662eaadc78b915a65b6aa1844ede05b0c18e3
Piyush Verma <piyushsuhani05@gmail.com>
2026-09-07 14:06:41 +0530

    Time: 2 ms (100.00%) | Memory: 47.9 MB (92.52%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
@@ -2,7 +2,8 @@ class Solution {
     public boolean searchMatrix(int[][] arr, int target) {
         int r = arr.length;
         int c = arr[0].length;
-        int i=0;
+
+        int i = 0;
         int j = c-1;
         while(i<r && j>=0)
         {
@@ -19,6 +20,7 @@ class Solution {
                 i++;
             }
         }
+
         return false;
     }
 }
