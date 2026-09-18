50b9d176572e8340624d780973d924ea9e84456d
Piyush Verma <piyushsuhani05@gmail.com>
2026-09-18 10:29:15 +0530

    Time: 170 ms (5.21%) | Memory: 47.6 MB (87.76%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,16 +1,26 @@
 class Solution {
-    void swap(int[] arr, int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
+    public void moveZeroes(int[] arr) {
+        int n = arr.length;
+        int passes = 0;
+        for(int ele : arr)
+        {
+            if(ele==0)
+            {
+                passes++;
+            }
+        }
 
-    public void moveZeroes(int[] nums) {
-        int i = 0; // pointer for next non-zero placement
-        for (int j = 0; j < nums.length; j++) {
-            if (nums[j] != 0) {
-                swap(nums, i, j);
-                i++;
+
+        for(int i=0;i<passes;i++)
+        {
+            for(int j=0;j<n-1-i;j++)
+            {
+                if(arr[j]==0 && arr[j+1]!=0)
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
             }
         }
     }
